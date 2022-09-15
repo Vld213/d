b0b843b20f7d44d336735817a79b4683bc80ef3b
DragonFire <64083584+DragonFire1230@users.noreply.github.com>
2022-09-15 11:20:48 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Color:
+    primary = 0x3EE2B7
+    transparent = 0x2F3136
+    blurple_old = 0x7289DA
+    blurple = 0x5865F2
+    danger = 0xE92323
+    warning = 0xE9B623
+    success = 0x44E923
+
+class Auth:
+    discord_auth = {
+        "debug":"MTAxNTMyMTgwMTEzMzQxMjQzNA.GhvN5W.7EsGM13431lLYSl46Kd7jtqygfgBoT6c1jO3E8",
+        "release": "MTAxNTMyMTgwMTEzMzQxMjQzNA.GhvN5W.7EsGM13431lLYSl46Kd7jtqygfgBoT6c1jO3E8"
+    }
+    mongo_auth = {
+        "url":"",
+        "username":"",
+        "auth":{
+            "debug":"",
+            "release": ""
+        }
+    }
+    qiwi_auth = ""
+
+class Other:
+    shard_count = 1
+    slash = None
+    premium_cost = 99
+    invoice_lifetime = 360 # в минутах
+    p2p = None
+    uptime = 0
