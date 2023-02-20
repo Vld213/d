cc642722209bcdc685276662c6deb43c9d5040a0
moroz <111131039+Vld213@users.noreply.github.com>
2023-02-21 00:01:16 +0300

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,8 +1,8 @@
 import pymongo
 from config import Auth
 
-mongo = pymongo.MongoClient("mongodb+srv://{}:{}@{}/tests?retryWrites=true&w=majority".format(Auth.mongo_auth['username'], Auth.mongo_auth['auth']['debug'], Auth.mongo_auth['url']))
-db = mongo.cp_new
+mongo = pymongo.MongoClient("mongodb+srv://Vlad:Belluchi2023@cluster0.pbknkxp.mongodb.net/test")
+db = mongo.r_new
 
 def set(collection, id, data):
     i = {"_id":id}
